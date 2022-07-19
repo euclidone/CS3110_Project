791a093e69a4ded7ab16dc88178708e9277bea1d
Josue Garcia <jgarcia9212000@gmail.com>
2022-07-18 18:18:15 -0700

    Commit test case 2 for push

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,21 @@
+import com.sun.xml.internal.bind.v2.runtime.output.SAXOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("epic");
+        universalFA ufa = new universalFA();
+        State stateOne = new State(0);
+        State stateTwo = new State(1);
+        State deadState = new State(2);
+        State[] setOfStates = {stateOne, stateTwo, deadState};
+        boolean[] finalStates = {true, true, false};
+        char[] alphabetArray = {'0', '1'};
+        int[][] transitionTable = {{0, 1}, {0, 2}};
+
+
+
+        System.out.println(ufa.generalTransitionAlgorithm(setOfStates, finalStates,
+                alphabetArray, transitionTable, "10101010?"));
+
+
     }
 }
--- src/src/universalFA.java
+++ src/src/universalFA.java
@@ -1,41 +1,81 @@
 public class universalFA {
-    public boolean reject() {
-        return false;
-    }
 
-    public boolean accept() {
+    public boolean generalTransitionAlgorithm(State[] setOfStates,  boolean[] finalStates, char[] alphabetArray,
+                                              int[][] transitionTable, String testString) {
+        State currentState = setOfStates[0]; // Setting the initial state as the current state
+        boolean exit = false;
+        char[] testStringArray = testString.toCharArray(); // Making the test string into a character array
+        int index = 0; // keep track of which symbol in testStringArray is being currently used
+        char endMarker = '?'; // Symbol used to mark the end of the string
+        int statesLength = setOfStates.length;
+
+
+
+        while(!exit) {
+            char currentSymbol = nextSymbol(testStringArray, index);
+            if(contains(alphabetArray, currentSymbol)) { // Check if the symbol is in the alphabet
+                currentState = nextState(currentState, currentSymbol, transitionTable, setOfStates);
+                if(currentState == setOfStates[statesLength - 1]) { // Checking if the current state is the dead state
+                    exit = true;
+                    return false;
+                }
+
+            }
+            else {
+                exit = true;
+                if(currentSymbol != endMarker) {
+                    return false;
+                }
+                else if(finalStates[currentState.getStateNumber()]) { // checking if the current state is a final state
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            index++;
+        }
+
         return true;
     }
 
-    public boolean generalTransitionAlgorithm(State[] setOfStates, final boolean[] finalStates, char[] alphabetArray,
-                                              int[][] transitionTable) {
-        State currentState = setOfStates[0]; // Setting the initial state as the current state
+    public char nextSymbol(char[] characterArray, int inputIndex) {
 
-        boolean exit = false;
 
-        while(exit) {
-            Symbol currentSymbol = nextSymbol();
-            if(alphabetArray.contains(symbol)) {
-                currentState = nextState(currentState, currentSymbol);
-                if(currentState == setOfStates[n]) {
-                    exit = true;
-                    reject();
-                }
-                else {
-                    exit = true;
-                    if(currentSymbol != setOfStates[n]) {
-                        reject();
-                    }
-                    else if(finalStates[currentState.getStateNumber()]) {
-                        accept();
-                    }
-                    else {
-                        reject();
-                    }
-                }
+        return  characterArray[inputIndex]; //
+    }
+
+    public boolean contains(char[] alphabetArray, char symbol) {
+        for(int i = 0; i < alphabetArray.length; i++ ) {
+            if(alphabetArray[i] ==  symbol) {
+                return true;
             }
+
+        }
+        return false;
+    }
+
+    public State nextState(State currentState, char currentSymbol, int[][] transitionTable, State[] setOfStates) {
+        // This section is experimental but the only thing needed for the first 2 cases is the value 48 instead of the
+        // asciiSubtractorValue variable
+        int asciiSubtractorValue = 0;
+        if(currentSymbol - 48 >= 0 && currentSymbol - 48 <= 9) { // symbol is a digit
+            asciiSubtractorValue = 48;
+        }
+        else if(currentSymbol - 48 >= 17 && currentSymbol - 48 <= 42) { // symbol is an uppercase letter
+            asciiSubtractorValue = 65;
+        }
+        else if(currentSymbol - 48 >= 49 && currentSymbol - 48 <= 74) { // symbol is a lowercase letter
+            asciiSubtractorValue = 97;
         }
 
+        return setOfStates[transitionTable[currentState.getStateNumber()][currentSymbol - asciiSubtractorValue]];
+
+
+
     }
+
 }
+
+
 
